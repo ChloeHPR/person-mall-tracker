497042bc718e2f246b5c60a3e7ed6a529b4743ac
ChloeHPR <chloe.houpert@gmail.com>
2026-09-06 20:07:11 +0200

    Implement gender evaluation for screenshots
    Add gender input processing for cropped images.

--- tracking.py
+++ tracking.py
@@ -62,6 +62,10 @@ while cap.isOpened():
                 crop = pil_image.crop((x1, y1, x2, y2))
                 
                 ### evaluate attributes
+                inputs_gender = clip_processor(text=GENDER_choixe, images=crop, return_tensors="pt", padding=True).to(device)
+                
+                
 ### save score and most matched person
-
+              
+    
 cap.release()
